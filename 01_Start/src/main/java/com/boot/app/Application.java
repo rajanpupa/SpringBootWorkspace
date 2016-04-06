c2ed50ff663cb36b365613995196330315c5260d
Rajan <rajanpupa@gmail.com>
2016-04-06 11:20:56 -0400

    re-factored the code, added Greetings controller to return a collection of objects

--- 01_Start/src/main/java/com/boot/app/Application.java
+++ 01_Start/src/main/java/com/boot/app/Application.java
@@ -1,6 +1,10 @@
-package com.boot.controller;
+package com.boot.app;
 
 import java.util.Arrays;
+
+/**
+ * Spring Boot main Application
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +14,7 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
+    	// this line is all you need for the basic Spring boot project
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
